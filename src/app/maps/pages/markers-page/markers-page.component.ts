c42cf69dada196a651e2883479bea1973058476a
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-09 17:43:35 +0200

    Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrl: './markers-page.component.css'
+  styleUrl: './markers-page.component.css',
 })
 export class MarkersPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -3.703288185161739,
+    40.41714755304062
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    //const markerHtml = document.createElement('div');
+    //markerHtml.innerHTML = 'Pointer';
+    //
+    //const marker = new Marker({
+    //  // color: 'red',
+    //  element: markerHtml,
+    //})
+    //  .setLngLat(this.currentLngLat)
+    //  .addTo(this.map);
+
+    const marker = new Marker().setLngLat(this.currentLngLat).addTo(this.map);
+  }
+
+  createMarker() {}
 }

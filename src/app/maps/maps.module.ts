c1d984dcc2b39ca9f3ef09153c636ed0b71eccf1
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-10 12:06:08 +0200

    Componente tradicional a un Standalone

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -8,7 +8,7 @@ import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
@@ -18,14 +18,18 @@ import { CounterAloneComponent } from '../alone/components/counter-alone/counter
 
 @NgModule({
   declarations: [
-    MiniMapComponent,
-    SideMenuComponent,
-    MapsLayoutComponent,
     FullScreenPageComponent,
+    MapsLayoutComponent,
     MarkersPageComponent,
+    MiniMapComponent,
     PropertiesPageComponent,
     ZoomRangePageComponent,
   ],
-  imports: [CommonModule, MapsRoutingModule, CounterAloneComponent],
+  imports: [
+    CommonModule,
+    CounterAloneComponent,
+    MapsRoutingModule,
+    SideMenuComponent,
+  ],
 })
 export class MapsModule {}
